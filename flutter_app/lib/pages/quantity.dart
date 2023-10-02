18494a4f9b95647ae1c59614433244209f905629
UmerGhaus <muhammadumerghaus@gmail.com>
2023-10-02 15:13:43 +0500

    Add Flutter App

---
+++ flutter_app/lib/pages/quantity.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class Quantity extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Quantity> {
+  final GlobalKey<FormState> _formStateKey = GlobalKey<FormState>();
+  Order _order = Order(quantity: 0);
+
+  String? _validateItemRequired(String? value) {
+    return value?.isEmpty == true ? 'Item Required' : null;
+  }
+
+  String? _validateItemCount(String? value) {
+    int? _valueAsInteger =
+        value != null && value.isNotEmpty ? int.tryParse(value) : null;
+    return _valueAsInteger == null || _valueAsInteger == 0
+        ? 'At least one Item is Required'
+        : null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Form(
+              key: _formStateKey,
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Espresso',
+                        labelText: 'Item',
+                      ),
+                      validator: (value) => _validateItemRequired(value),
+                      onSaved: (value) => _order.item = value,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: '3',
+                        labelText: 'Quantity',
+                      ),
+                      validator: (value) => _validateItemCount(value),
+                      onSaved: (value) {
+                        _order.quantity = int.tryParse(value ?? '0') ?? 0;
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _submitOrder,
+              child: Text('Submit Order'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _submitOrder() {
+    final formState = _formStateKey.currentState;
+    if (formState != null && formState.validate()) {
+      formState.save();
+      final snackBar = SnackBar(
+        content: Text(
+            'Order Item: ${_order.item}, Order Quantity: ${_order.quantity}'),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
+}
+
+class Order {
+  String? item;
+  int quantity;
+
+  Order({this.item, required this.quantity});
+}
